fa6e190beef8f9f9f5a81238ae83fec62c1c580a
Rémy <Remypottierpro@gmail.com>
2019-10-04 14:54:35 +0200

    pomme

--- app.js
+++ app.js
@@ -24,21 +24,43 @@ function addInput() {
     input.after(subDiv)
     createSubInput(subDiv)
 }
-function createSubInput(subDiv){
+
+function createSubInput(subDiv) {
     // Creation of 3 inputs
-    for(let i =0;i<3;i++){
+    for (let i = 0; i < 3; i++) {
+        let thirdDiv = document.createElement('div')
+        thirdDiv.classList.toggle('thirdDiv')
         let subInput = document.createElement('input')
+        let valueInput = document.createElement('input')
         // Class
         subInput.classList.toggle('subInput')
+        valueInput.classList.toggle('valueInput')
         // Attribute
         subInput.setAttribute('type', 'text')
+        valueInput.setAttribute('type', 'number')
         subInput.setAttribute('placeholder', 'Enter proposition of question')
-        subInput.setAttribute('name', numberOfInput*10+1)
+        valueInput.setAttribute('value', '10')
         // Creation of element
-        subDiv.append(subInput)
+        thirdDiv.append(subInput)
+        thirdDiv.append(valueInput)
+        subDiv.append(thirdDiv)
     }
 }
 
 function sendInputs() {
-    const 
+    const queryPrimaryDiv = document.querySelectorAll('.primaryDiv')
+    const arrayQueryPrimaryDiv = Array.from(queryPrimaryDiv)
+    let datas = {}
+    let result = arrayQueryPrimaryDiv.map(x => {
+        let inputsValues = {}
+        inputsValues.element = x
+        let inputQuestion = x.querySelector('.input--proposition');
+        inputsValues.inputQuestionValue = inputQuestion.value;
+        inputsValues.arrayInputOptions = Array.from(x.querySelectorAll('.subInput'));
+        inputsValues.inputOptions = inputsValues.arrayInputOptions.map(y =>{let valueInput = y.value; return valueInput});
+        inputsValues.arrayOptionValue = Array.from(x.querySelectorAll('.valueInput'));
+        inputsValues.optionValue = inputsValues.arrayOptionValue.map(y =>{let valueInput = y.value; return valueInput});
+        return inputsValues
+    });
+    console.log(result)
 }
